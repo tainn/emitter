430fb1e4c65bb23023adf52be49539ca9890d350
tainn <tainn@protonmail.com>
2024-11-03 21:23:25 +0100

    to emitter and git network

--- src/main.py
+++ src/main.py
@@ -10,35 +10,35 @@ def main() -> None:
 
     while True:
         # monday: 0, sunday: 6
-        if str(datetime.now().weekday()) not in os.getenv("RECP_SKIP_DAYS"):
+        if str(datetime.now().weekday()) not in os.getenv("EMITTER_SKIP_DAYS"):
             repeat()
             print(f"pushed at {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}")
         else:
             print(f"skipped {datetime.now().strftime("%A").lower()} at {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}")
 
-        print(f"sleeping for {os.getenv("RECP_SLEEP_SECS")} secs at {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}")
-        time.sleep(int(os.getenv("RECP_SLEEP_SECS")))
+        print(f"sleeping for {os.getenv("EMITTER_SLEEP_SECS")} secs at {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}")
+        time.sleep(int(os.getenv("EMITTER_SLEEP_SECS")))
 
 
 def init() -> None:
-    repo_path = f"/tmp/{os.getenv("RECP_REPO_NAME")}"
+    repo_path = f"/tmp/{os.getenv("EMITTER_REPO_NAME")}"
 
     Path(repo_path).mkdir(parents=True, exist_ok=True)
     os.chdir(repo_path)
 
     sp.run("git init", shell=True)
 
-    sp.run(f"git config --local user.name '{os.getenv("RECP_USER_NAME")}'", shell=True)
-    sp.run(f"git config --local user.email '{os.getenv("RECP_USER_EMAIL")}'", shell=True)
+    sp.run(f"git config --local user.name '{os.getenv("EMITTER_USER_NAME")}'", shell=True)
+    sp.run(f"git config --local user.email '{os.getenv("EMITTER_USER_EMAIL")}'", shell=True)
 
-    sp.run(f"git remote add origin '{os.getenv("RECP_REPO_AUTH_URL")}'", shell=True)
-    sp.run(f"git checkout -b '{os.getenv("RECP_BRANCH")}'", shell=True)
+    sp.run(f"git remote add origin '{os.getenv("EMITTER_REPO_AUTH_URL")}'", shell=True)
+    sp.run(f"git checkout -b '{os.getenv("EMITTER_BRANCH")}'", shell=True)
 
 
 def repeat() -> None:
-    sp.run(f"git pull origin '{os.getenv("RECP_BRANCH")}'", shell=True)
+    sp.run(f"git pull origin '{os.getenv("EMITTER_BRANCH")}'", shell=True)
     sp.run("git commit --allow-empty -m 'echo'", shell=True)
-    sp.run(f"git push origin '{os.getenv("RECP_BRANCH")}'", shell=True)
+    sp.run(f"git push origin '{os.getenv("EMITTER_BRANCH")}'", shell=True)
 
 
 if __name__ == "__main__":
